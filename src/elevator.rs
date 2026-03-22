c5432e3142b567e76e7e95ca56d51f49e687c0ec
TinaGrim <195917429+TinaGrim@users.noreply.github.com>
2026-03-22 15:24:42 +0700

    elevator: add floor tracking and door status methods

--- src/elevator.rs
+++ src/elevator.rs
@@ -1,13 +1,13 @@
 use egui::{self, Options, TextureHandle, TextureId, Vec2};
 use std::fmt;
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum ActionStatus {
     Stop,
     Idle,
     Up,
     Down,
 }
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Door {
     Close,
     Open,
@@ -15,8 +15,8 @@ pub enum Door {
 // #[derive(Debug)]
 pub struct ElevatorObject {
     id: u32,
-    status: ActionStatus,
-    door: Door,
+    pub status: ActionStatus,
+    pub door: Door,
 
     // position
     x: f32,
@@ -24,6 +24,7 @@ pub struct ElevatorObject {
     velocity_x: f32,
     velocity_y: f32,
     destination: f32,
+    floor: String,
 
     speed: f32,
     image: Option<TextureHandle>,
@@ -38,13 +39,14 @@ impl fmt::Debug for ElevatorObject {
             .field("y", &self.y)
             .field("velocity_x", &self.velocity_x)
             .field("velocity_y", &self.velocity_y)
+            .field("Floor", &self.floor)
             .field("Speed", &self.speed)
             .field("Image", &self.image.is_some()) // Show bool instead
             .finish()
     }
 }
 impl ElevatorObject {
-    pub fn new(id: u32, x: f32, y: f32) -> Self {
+    pub fn new(id: u32, floor: String, x: f32, y: f32) -> Self {
         Self {
             id,
             status: ActionStatus::Idle,
@@ -53,6 +55,7 @@ impl ElevatorObject {
             y,
             velocity_x: 0.0,
             velocity_y: 0.0,
+            floor,
             destination: 0.0,
 
             speed: 0.0,
@@ -74,6 +77,15 @@ impl ElevatorObject {
     pub fn set_destination(&mut self, y: f32, available: egui::Rect) {
         self.destination = available.min.y.round() + y;
     }
+    pub fn set_floor(&mut self, floor: String) {
+        self.floor = floor
+    }
+    pub fn get_current_floor(&self) -> String {
+        self.floor.clone()
+    }
+    pub fn reach_destination(&mut self) -> bool {
+        self.y == self.destination
+    }
     pub fn get_position(&self) -> (f32, f32) {
         (self.x, self.y)
     }
@@ -82,6 +94,12 @@ impl ElevatorObject {
     }
     pub fn texture_id(&self) -> egui::TextureId {
         self.image.clone().unwrap().id()
+    }
+    pub fn door_status(&self) -> Door {
+        match self.door {
+            Door::Close => Door::Close,
+            Door::Open => Door::Open,
+        }
     }
     pub fn toggle_door(&mut self) -> Door {
         self.door = match self.door {
